a8dc4e1e96d76e8a4f24b55f668c6fd33b09d62f
rayhan <rayhanaulia@students.amikom.ac.id>
2021-03-17 22:34:08 +0700

    menambahkan tugas classtaxi

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriverName = "Rayhan";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassager();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxi
+{
+    public class Taxi
+    {
+        // properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float  NumPassenger { get; set; }
+
+        //method
+        public void PickUpPassager()
+        {
+            Console.WriteLine("Drive Name: {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDuty ?"Yes" : "No");
+            Console.WriteLine("Number of Passenger: {0}\n", NumPassenger);
+            Console.WriteLine("sedang menjemput penumpang");
+
+        }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Taxi Info");
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("selesai mengantar penumpang");
+        }
+            
+
+    }
+}
